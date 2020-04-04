d21fcca9b95be1c629c9a7824d2f14bb081ad90e
Robert Link <rl8z@virginia.edu>
2020-04-04 13:01:16 -0400

    Refactor data prep code out of gen_likelihood()

--- R/likelihood.R
+++ R/likelihood.R
@@ -45,45 +45,12 @@
 gen_likelihood <- function()
 {
   ## silence warnings
-  ntest <- ftest <- state <- date <- FIPS <- fips <- 
-    cases <- day <- Locality <- locality <- newCases <- newcases <- NULL
+  locality <- newCases <- ftest <- NULL
   
-  ## First get the datasets.  Filter and reformat as necessary.
-  teststats <- 
-    dplyr::filter(uvads_covid19, !is.na(ntest)) %>%
-    dplyr::select(date, ftest)
-  
-  obsdata <- 
-    dplyr::filter(NYTimesCOVID19::cov19county, 
-                  state=='Virginia',
-                  date >= min(teststats[['date']])) %>%
-    dplyr::mutate(day = as.numeric(date - as.Date('2020-01-01')),
-                  fips = as.integer(fips)) %>%
-    dplyr::group_by(fips) %>%
-    dplyr::mutate(newcases = c(0, diff(cases))) %>%    # add daily new cases
-    dplyr::ungroup() %>%
-    ## sometimes you get a decrease due to transfers or data corrections.  Treat
-    ## these as zero
-    dplyr::mutate(newcases = pmax(0, newcases)) %>%
-    dplyr::left_join(teststats, by='date') %>%
-    dplyr::select(date, fips, newcases, ftest, day)
-    
-  ## Our locality names are not the same as NYT, but we have a table of FIPS codes
-  fips_codes <- dplyr::select(va_county_first_case, FIPS, Locality) %>%
-    dplyr::rename(fips=FIPS)
-  
-  ## Ordinarily I would arrange the obs in the same order that the output comes
-  ## from the model, but in this case we can't be guaranteed that we will have
-  ## obs for every county at every model time, so we're just going to have to 
-  ## do a join after each model run.
-  
-  ## Default values for likelihood parameters.  Ideally we should collect all of
-  ## the parameter defaults together somewhere.
-  
-  default_parm_vals <- c(
-    day_zero = 60,
-    b = 0.5
-  )
+  obs <- get_obsdata()
+  obsdata <- obs$obsdata
+  fips_codes <- obs$fips_codes
+  default_parm_vals <- obs$default_parm_vals
   
   function(parms) {
     ## The day-zero parameter is being handled in a simplified way compared to
@@ -157,4 +124,65 @@ gen_post <- function()
     }
     logp
   }
+}
+
+#' Prepare the observed data for use in the likelihood function
+#' 
+#' @return List of observations used in the likelihood function.  This includes
+#' \describe{
+#'   \item{obsdata}{Confirmed new cases by date and county.  Also includes 
+#'   statewide fraction of people tested.}
+#'   \item{fips_codes}{Lookup table of FIPS codes for the Locality names output by
+#'   the model.}
+#'   \item{default_parm_vals}{Default values for the parameters used to compare
+#'   model output to observed data (the parameters used inside the model have their
+#'   own defaults)}
+#' }
+#' 
+#' @keywords internal
+get_obsdata <- function()
+{
+  ## silence warnings
+  ntest <- ftest <- state <- date <- FIPS <- fips <- 
+    cases <- day <- Locality <- newcases <- NULL
+  
+  ## First get the datasets.  Filter and reformat as necessary.
+  teststats <- 
+    dplyr::filter(uvads_covid19, !is.na(ntest)) %>%
+    dplyr::select(date, ftest)
+  
+  obsdata <- 
+    dplyr::filter(NYTimesCOVID19::cov19county, 
+                  state=='Virginia',
+                  date >= min(teststats[['date']])) %>%
+    dplyr::mutate(day = as.numeric(date - as.Date('2020-01-01')),
+                  fips = as.integer(fips)) %>%
+    dplyr::group_by(fips) %>%
+    dplyr::mutate(newcases = c(0, diff(cases))) %>%    # add daily new cases
+    dplyr::ungroup() %>%
+    ## sometimes you get a decrease due to transfers or data corrections.  Treat
+    ## these as zero
+    dplyr::mutate(newcases = pmax(0, newcases)) %>%
+    dplyr::left_join(teststats, by='date') %>%
+    dplyr::select(date, fips, newcases, ftest, day)
+    
+  ## Our locality names are not the same as NYT, but we have a table of FIPS codes
+  fips_codes <- dplyr::select(va_county_first_case, FIPS, Locality) %>%
+    dplyr::rename(fips=FIPS)
+  
+  ## Ordinarily I would arrange the obs in the same order that the output comes
+  ## from the model, but in this case we can't be guaranteed that we will have
+  ## obs for every county at every model time, so we're just going to have to 
+  ## do a join after each model run.
+  
+  ## Default values for likelihood parameters.  Ideally we should collect all of
+  ## the parameter defaults together somewhere.
+  
+  default_parm_vals <- c(
+    day_zero = 60,
+    b = 0.5
+  )
+  
+  list(obsdata=obsdata, fips_codes=fips_codes, 
+       default_parm_vals=default_parm_vals)
 }
