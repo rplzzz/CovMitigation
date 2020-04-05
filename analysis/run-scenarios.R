51557b71faf753546e9e54bae1252cb2bbbf8f55
Robert Link <rl8z@virginia.edu>
2020-04-05 03:39:13 -0400

    Add script to generate scenarios for hospital census model

---
+++ analysis/run-scenarios.R
@@ -0,0 +1,23 @@
+library(magrittr)
+library(CovMitigation)
+
+T0vals <- seq(4.5,7.5)
+baseparms <- list()
+
+basescens <- 
+  dplyr::bind_rows(
+    lapply(T0vals, function(T0) {
+      parms <- c(baseparms, list(T0=T0))
+      run_scenario(270, parms, paste('td = ', T0))
+    }))
+
+pltdata <- basescens %>%
+  dplyr::group_by(time, scenario) %>%
+  dplyr::summarise(
+    newCases = sum(newCases*marketFraction),
+    PopSympto = sum(PopSympto*marketFraction),
+    PopInfection = sum(PopInfection*marketFraction),
+    PopCumulInfection = sum(PopCumulInfection*marketFraction),
+    PopCumulFrac = sum(PopCumulInfection)/sum(population*marketFraction),
+    popTotal = sum(population*marketFraction)
+  )
