071f7216de82604027f18cd28a1ee08ac42e0144
Robert Link <rl8z@virginia.edu>
2020-04-09 22:47:48 -0400

    Fix typo in run-mcmc.R

--- inst/scripts/run-mcmc.R
+++ inst/scripts/run-mcmc.R
@@ -5,7 +5,7 @@ library(doParallel)
 
 #pstrt <- c(T0 = 3.1, D0 = 4.9, A0 = 3.5, day_zero = 37.1, b = 3.1, I0=2.7)
 pstrt <- c(T0 = 7.5, D0 = 4.5, A0 = 5.0, day_zero = 3.9, b = 8.7, I0=24.8)
-scl <- c(0.1, 0.1, 0.1, 1.0, 1.0)
+scl <- c(0.1, 0.1, 0.1, 1.0, 1.0, 1.0)
 
 lpost <- gen_post()
 
