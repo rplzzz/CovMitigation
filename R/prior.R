476846aa8a8a987bebf84b6f9e669036647ef71d
Robert Link <rl8z@virginia.edu>
2020-04-11 14:18:04 -0400

    Adjust prior for 'b' parameter

--- R/prior.R
+++ R/prior.R
@@ -35,7 +35,7 @@ gen_prior <- function()
         dgamma(parms[c('T0', 'A0', 'Ts')], c(3, 8,8), c(0.5, 2,2), log=TRUE),
         dlnorm(parms['D0'], 2, 0.5, log=TRUE),
         dnorm(parms['day_zero'], 50, 15, log=TRUE),
-        dlnorm(parms['b'], 2.5, 1.5, log=TRUE),
+        dlnorm(parms['b'], 2.5, 1, log=TRUE),
         dlnorm(parms['I0'], 2, 1, log=TRUE)
       )
       sum(logps, na.rm = TRUE)
--- analysis/mcmc.R
+++ analysis/mcmc.R
@@ -16,23 +16,20 @@ lpost <- gen_post(fixed_parms=p0)   # fixed parameters can still be overriden by
 print(lpost(p0))
 
 # Optimize with D0 and A0 fixed at their default values
-pstrt <- c(D0=7, day_zero=60, b=20, I0=10)
+pstrt <- c(D0=7, A0=5, day_zero=10, b=10, I0=10)
 opt <- optim(pstrt, lpost, control=list(fnscale=-1, maxit=1000))
 
-
-set.seed(867-5309)
-
+opt_uncons <- optim(c(T0=7.5, opt$par), lpost, control=list(fnscale=-1, maxit=1000))
 
 ## Based on results of opt, above (caching these values to prevent having to 
 ## rerun it, since it takes about 45 minutes)
-popt <- c(T0=7.5, D0=4, A0=5, day_zero=2.7, b=2.9, Ts=4, I0=26)
+popt <- c(T0=7.5, D0=3.4, A0=2.9, day_zero=5.9, b=4.13, I0=22.1, Ts=4)
+popt_uncons <- c(T0=2.7, D0=3.6, A0=3.6, day_zero=39.2, b=1.1, I0=4.1, Ts=4)
 
-pmat <- matrix(c(p0,popt), nrow=2, byrow = TRUE)
-colnames(pmat) <- names(p0)
-plt_projections(pmat, c('Base Scenario', 'Opt Scenario'), usedate=TRUE)
+pmat <- rbind(popt, popt_uncons)
+plt_projections(pmat, c('Constrained td=7.5', 'Unconstrained'), usedate=TRUE)
 
-p1 <- c(T0=7.5, D0=4.5, A0=5, day_zero=3.9, b=8.7, I0=24.8)
-scl0 <- c(0.1, 0.1, 0.1, 1, 1, 1)
+scl0 <- c(0.1, 0.1, 0.1, 1, 1, 1, 0.1)
 
 plot_traces <- function(ms) {
   pltdata <- as.data.frame(ms$samples)
@@ -41,16 +38,12 @@ plot_traces <- function(ms) {
   ggplot(pltdata, aes(x=iter, y=value)) + geom_line() + facet_wrap(~parm, scales='free')
 }
 
-ms1 <- metrosamp(lpost, p1, 100, 1, scl0)
-ms2 <- metrosamp(lpost, ms1, 100,1, scl0*2)
-ms3 <- metrosamp(lpost, ms2, 100, 1, scl0*5)
-ms4 <- metrosamp(lpost, ms3, 100, 1, scl0*2)
-ms5 <- metrosamp(lpost, ms4, 100, 1, scl0)
+set.seed(867-5309)
+ms1 <- metrosamp(lpost, popt_uncons, 100, 1, scl0)
+ms2 <- metrosamp(lpost, ms1, 100,1, scl0/5)
+ms3 <- metrosamp(lpost, ms2, 100, 1, scl0/5)
+ms4 <- metrosamp(lpost, ms3, 100, 1, scl0/2)
+ms5 <- metrosamp(lpost, ms4, 100, 1, scl0/2)
 
-ms6 <- metrosamp(lpost, ms5, 1000, 1, scl0)
-scl1 <- scl0
-scl1[4] <- 5
-scl1[5] <- 5
-scl1[6] <- 2
-ms7 <- metrosamp(lpost, ms6, 1000, 1, scl1)
+ms6 <- metrosamp(lpost, ms5, 1000, 1)
 
