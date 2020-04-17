db592b7eb2c0ec0f726e2d18e2d3dcad3ff9666c
Robert Link <rl8z@virginia.edu>
2020-04-17 13:45:54 -0400

    Update input data

--- data-raw/update-uvads.R
+++ data-raw/update-uvads.R
@@ -33,6 +33,6 @@ usethis::use_data(uvads_covid19, overwrite=TRUE)
 ## Hospitalization dataset
 uva_covid_count <- readr::read_csv(here('data-raw','COVID_counts_uva.csv'), 
                                    col_types = 'ciiiiiiiii')
-uva_covid_count$date <- lubridate::mdy(uva_covid_count$date)
+uva_covid_count$date <- lubridate::mdy(uva_covid_count$Day)
 uva_covid_count$time <- as.numeric(uva_covid_count$date - as.Date('2020-01-01'))
 usethis::use_data(uva_covid_count, overwrite=TRUE)
