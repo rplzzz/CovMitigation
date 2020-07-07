8355e1bbdf574a0e040657a54d0d7d2919d7441f
Robert Link <rl8z@virginia.edu>
2020-07-07 13:53:25 -0400

    Add an optimization with mobility parameter suppressed.

--- analysis/opt-parm.R
+++ analysis/opt-parm.R
@@ -10,8 +10,8 @@ registerDoParallel(8)
 viscounties <- c('AlbemarleCounty', 'Charlottesvillecity', 'NelsonCounty',  ## Thomas Jefferson district
                  'FairfaxCounty', 'ArlingtonCounty', 'PrinceWilliamCounty',  ## Northern Virginia
                  'Richmondcity', 'HenricoCounty', 'ChesterfieldCounty'      ## Richmond area
-                 )
-  
+)
+
 day0fix <- 30
 
 p0 <- c(eta=-0.7, xi=1, zeta=0, D0=7, A0=5, day_zero=day0fix, b=20, I0=10, Ts=4)
@@ -25,11 +25,17 @@ ctrl <- list(fnscale=-1, maxit=2000)
 opt_uncons <- optim(pstrt, lpost, control=ctrl)
 puncons <- opt_uncons$par
 
-pmat <- rbind(puncons)
-scen_names <- c('Unconstrained')
+pstrt2 <- puncons[-c(3)]
+opt_nomob <- optim(pstrt2, lpost, control=ctrl)
+pnomob <- opt_nomob$par
+pnomob <- c(pnomob[1:2], zeta=0, pnomob[3:7])
+
+pmat <- rbind(puncons, pnomob)
+scen_names <- c('Mobility adjust', 'No mobility adjust')
 
 proj <- plt_projections(pmat, scen_names, p0, usedate=TRUE) + scale_color_brewer(type='qual')
 print(proj)
 
 modobs <- plt_modobs(pmat, scen_names, viscounties) + ggplot2::scale_color_brewer(type='qual')
 print(modobs)
+
