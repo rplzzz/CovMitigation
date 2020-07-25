1284c71d1066bceb7762aedb96b2ac32a072f583
Robert Link <rl8z@virginia.edu>
2020-07-25 15:32:44 -0400

    Update Bayesian functions to use county-specific beta parameters.

--- R/likelihood.R
+++ R/likelihood.R
@@ -1,16 +1,16 @@
 
 #' Prepare a likelihood function for use in Bayesian calibration
-#' 
-#' Build a function that given a set of model parameters runs the model and 
+#'
+#' Build a function that given a set of model parameters runs the model and
 #' computes the log-likelihood for the model.
-#' 
+#'
 #' The observational data is the daily county-by-county reports of new confirmed
 #' COVID-19 cases.  We are using the New York Times dataset
 #' (\code{https://github.com/nytimes/covid-19-data}), which is based on reports
 #' from state departments of health, including the VDH.  For statistics on COVID-19
 #' testing, we use the \code{\link[vacovdata]{vacovdata}} dataset, which was compiled
 #' by the COVID Tracking project (\code{https://covidtracking.com/}).
-#' 
+#'
 #' The model parameters currently recognized are:
 #' \describe{
 #' \item{eta}{(real) Log of the baseline transmissibility}
@@ -26,28 +26,28 @@
 #' \item{I0}{(real > 0) The initial number of infected people, once the infection starts.
 #' This is taken to be the same in all counties.}
 #' }
-#' For the time being, we start tracking the infection in Fairfax county on day 30, and 
+#' For the time being, we start tracking the infection in Fairfax county on day 30, and
 #' all of the counties are delayed relative to Fairfax by a number of days equal
 #' to the difference between their first observed case and the first case observed
 #' in Fairfax.
-#' 
-#' To compute the likelihood, our assumption is that the output of the model 
+#'
+#' To compute the likelihood, our assumption is that the output of the model
 #' represents an average infection rate.  The average observation rate is then
 #' \deqn{\bar{N}_o = \bar{N}_I f_t b,} where \eqn{f_t} is the fraction of the total
 #' population tested.  We then assume that the actual observations are distributed
 #' as \deqn{N_o \sim Pois(\bar{N}_o).}
-#' 
+#'
 #' @section Likelihood function:
-#' 
+#'
 #' By default, the function produced will return a single value, which is the sum of
 #' \itemize{
-#' \item{The log-likelihood function, summed over all data points} 
+#' \item{The log-likelihood function, summed over all data points}
 #' \item{A correction for the hospitalization fraction}
 #' \item{A correction for the symptomatic fraction}
 #' }
 #' Technically, the latter two are priors, since they don't depend on the observations,
 #' but the model must be run in order to compute them, so they are computed here.
-#' 
+#'
 #' If the \code{waicmode} flag is set, then the function will return detailed information
 #' on the contribution of each data point to the log-likelihood.  The result will be a
 #' data frame containing
@@ -60,34 +60,34 @@
 #' \item{log likelihood}
 #' }
 #' Additionally, the hospitalization fraction and symptomatic fraction corrections
-#' will be attached to the data frame as attributes (\code{hfadjust} and 
+#' will be attached to the data frame as attributes (\code{hfadjust} and
 #' \code{fsadjust}).
-#' 
+#'
 #' The default mode output can be used in optimizations or Markov chain Monte Carlo.
 #' The waicmode output is useful for computing the WAIC, or for diagnosing which
 #' counties and/or days are favoring one model over another.
-#' 
+#'
 #' @param hparms Hyperparameters for the calculation, as described in \code{\link{gen_post}}
 #' @param fixed_parms A named vector of parameters to use as fixed values for parameters
 #' not passed to the likelihood function.  Note these "fixed" parameters can still
 #' be overridden by passing an explicit value.
 #' @param verbose If \code{TRUE}, print additional diagnostic information.
-#' @param waicmode If \code{TRUE}, generate a function that returns the likelihood 
+#' @param waicmode If \code{TRUE}, generate a function that returns the likelihood
 #' contributions from each data point.
 #' @return A function that takes a named list of model parameters and returns
-#' a log-likelihood value.  See details for the parameters recognized and the 
+#' a log-likelihood value.  See details for the parameters recognized and the
 #' output of the function returned.
 #' @importFrom dplyr %>%
 #' @export
 gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FALSE)
 {
   ## silence warnings
-  fips <- ntest <- biased_fi <- locality <- npos <- expected <- 
+  fips <- ntest <- biased_fi <- locality <- npos <- expected <-
     x <- m <- n <- k <- NULL
-  
+
   obs <- get_obsdata()
   obsdata <- obs$obsdata
-  
+
   if(!is.null(fixed_parms)) {
     for(p in names(fixed_parms)) {
       obs$default_parm_vals[[p]] <- fixed_parms[[p]]
@@ -95,24 +95,24 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
   }
   default_parm_vals <- obs$default_parm_vals
 
-  ## Final dates of the weeks we use for aggregation  
+  ## Final dates of the weeks we use for aggregation
   wkdates <- sort(unique(obs$obsdata$date))
   ## Table of fips codes
   fips_table <- unique(dplyr::select(obs$obsdata, locality, fips))
   jan01 <- as.Date('2020-01-01')
-  
+
   function(parms) {
     ## complete the parameters from the defaults
     parms <- fill_defaults(parms, default_parm_vals)
-    
+
     ## Separate out the parameters that get passed to the SEIR model.
     seirparms <- as.list(parms[! names(parms) %in% c('b0', 'b1')])
-    
+
     ## get output for every day up to the last in the dataset.
     tmax <- max(obsdata[['time']])
     t1 <- infection_t0             # constant defined in sim.R
     tvals <- seq(t1, tmax)
-    
+
     ## Run the model and do a little post-processing
     modout <- run_scenario(tvals, seirparms)
     if(nrow(modout)==0) {
@@ -126,7 +126,7 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
     mdata$fi <- mdata$Itot / mdata$population
     mdata <- dplyr::left_join(mdata, fips_table, by='locality')
     mdata$date <- mdata$time + jan01
-    
+
     ## Aggregate the data by week.  For model outputs we want the average over
     ## the week.
     mdatawk <-
@@ -137,48 +137,48 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
       },
       keep=TRUE) %>%
       dplyr::bind_rows()
-    
+
     cmp <- dplyr::full_join(obs$obsdata, mdatawk, by=c('time','date', 'locality', 'fips'))
     cmp <- cmp[!(is.na(cmp$fi) | is.na(cmp$ntest)),]
     cmp <- cmp[(cmp$fi > 0) & (cmp$ntest > 0),]
-    
+
     ## If any rows have Itot missing, it means that our day-zero put the start
     ## of the outbreak after the first observed case in that county.  Such parameter
     ## values have likelihood == 0.
     ## Conversely, if cases is missing, it means that we had a nonzero projection
     ## from the model on a day before we had any observed cases, which is perfectly
     ## fine, so fill in those cases with zeros.
-    
+
     if(any(is.na(cmp$Itot))) {
       -Inf
     }
     else {
       miss <- is.na(cmp$npos)
       cmp$npos[miss] <- 0
-      
+
       ## adjust model outputs for testing fraction and testing bias!
       ## Assume that the enrichment factor is never less than 1 (i.e., we never
       ## select *against* infection)
       b <- pmax(parms[['b0']] - parms[['b1']] * log(cmp$ntest), 1)
-      
+
       ## The model forecast for the number of new cases is the current infection
       ## fraction (fi) times the number of tests performed.  However, we think that
       ## testing is biased toward people who are infected, so we multiply the odds
       ## ratio by a bias factor b
       cmp$biased_fi <- padjust(cmp$fi, b)
-      
+
       dhargs <- tibble::tibble(x=cmp$npos,
                                m=cmp$biased_fi * cmp$population,
                                n=(1-cmp$biased_fi) * cmp$population,
                                k=cmp$ntest)
-      
+
       ## Sometimes you get a number of observations greater than the total number
       ## of cases the model is projecting.  In theory, that model should be excluded
       ## with a logl of -Inf, but that can make it hard to find a good initial guess.
       ## Instead, adjust x so that that x <= m, but apply a finite penalty for each x > m.
       dhpenalty <- -1000 * sum(dhargs$x > ceiling(dhargs$m))
       dhargs$x <- pmin(dhargs$x, ceiling(dhargs$m))
-      
+
       ## Hypergeometric distribution.  Assume that tests performed in each county
       ## are proportional to the county's share of the total population.
       logl <- dhyper(dhargs$x,
@@ -186,42 +186,42 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
                      ceiling(dhargs$n),
                      ceiling(dhargs$k),
                      log=TRUE)
-      
+
       if(any(is.na(logl))) {
         ## This seems to be happening occasionally.  Not sure why
         bad <- cmp[is.na(logl),]
         warning('Bad values in likelihood function.')
         for (i in seq(1,nrow(bad))) {
-          warning(paste(colnames(bad), collapse='\t'), '\n', 
+          warning(paste(colnames(bad), collapse='\t'), '\n',
                   paste(bad[i,], collapse='\t'))
         }
       }
-      
+
       ## Compute a likelihood term for the statewide symptomatic fraction, which
-      ## should be somewhere near 0.5.  Since this is just one value for the whole 
+      ## should be somewhere near 0.5.  Since this is just one value for the whole
       ## dataset, we weight it by the number of times in the data.  Note this term
       ## really only constrains Ts, and it is pretty much the only thing that constrains
       ## that parameter.
-      fsadjust <- 
-        (max(obsdata$time) - min(obsdata$time)) * 
+      fsadjust <-
+        (max(obsdata$time) - min(obsdata$time)) *
         dbeta(fs, hparms[['fsalpha']], hparms[['fsbeta']], log=TRUE)
-      
+
       ## Compute another adjustment for the hospitalization fraction.  Basically,
       ## if the model says that a significant portion of the community is infected,
       ## but we're only getting a trickle of hospital cases, then we don't believe
-      ## that.  
-      hospadjust <- hparms[['nhosp_weight']] * 
+      ## that.
+      hospadjust <- hparms[['nhosp_weight']] *
         nhosp_likelihood(hparms[['nhosp_alpha']], hparms[['nhosp_beta']], modout)
-      
+
       if(verbose) {
         suml <- signif(sum(logl), 5)
         fsa <- signif(fsadjust, 5)
         hsa <- signif(hospadjust, 5)
-        msg <- paste(c('logl:\t', 'fsadjust:\t', 'hospadjust:\t', 'dhpenalty:\t'), 
+        msg <- paste(c('logl:\t', 'fsadjust:\t', 'hospadjust:\t', 'dhpenalty:\t'),
                      c(suml, fsa, hsa, dhpenalty), collapse='\n')
         message('Likelihood contributions:\n', msg)
       }
-      
+
       if(waicmode) {
         rslt <- dplyr::bind_cols(cmp, dhargs) %>%
           dplyr::mutate(expected=biased_fi*ntest, logl=logl) %>%
@@ -229,7 +229,7 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
                         x, m, n, k, logl)
         attr(rslt, 'fsadjust') <- fsadjust
         attr(rslt, 'hfadjust') <- hospadjust
-        rslt          
+        rslt
       }
       else {
         sum(logl, na.rm=TRUE) + fsadjust + hospadjust + dhpenalty
@@ -239,9 +239,9 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
 }
 
 #' Prepare a posterior log-pdf function for use in Bayesian calibration
-#' 
+#'
 #' Create a function that computes and sums the log-prior and the log-posterior.
-#' 
+#'
 #' Recognized hyperparameters are
 #' \describe{
 #' \item{fsalpha}{Alpha parameter for the beta distribution of symptomatic fraction
@@ -249,8 +249,9 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
 #' \item{fsbeta}{Beta parameter for the beta distribution of symptomatic fraction in the
 #' likelihood.  Note the most likely value of fs is \eqn{\frac{\alpha-1}{\alpha+\beta-2}}}
 #' }
-#' 
-#' @param prior_weight Factor to multiply the prior by.  Default is to make it equal
+#'
+#' @param prior_weight Factor to multiply the prior by.  Default is 10.  If set
+#'   to \code{NULL}, make it equal
 #' to the number of counties for which we have confirmed COVID-19 cases.
 #' @param fixed_parms A named vector of parameters to use as fixed values for parameters
 #' not passed to the likelihood function.  Note these "fixed" parameters can still
@@ -260,18 +261,18 @@ gen_likelihood <- function(hparms, fixed_parms=NULL, verbose=FALSE, waicmode=FAL
 #' @param verbose If \code{TRUE}, have the prior and likelihood print additional
 #' diagnostic information.
 #' @export
-gen_post <- function(prior_weight=NULL, fixed_parms=NULL, hparms=list(), verbose=FALSE)
+gen_post <- function(prior_weight=10, fixed_parms=NULL, hparms=list(), verbose=FALSE)
 {
-  
+
   hparms <- fill_defaults(as.list(hparms), default_hparms)
-  
+
   lprior <- gen_prior(hparms, verbose=verbose)
   llik <- gen_likelihood(hparms, fixed_parms, verbose=verbose)
-  
+
   if(is.null(prior_weight)) {
     prior_weight <- sum(!is.na(va_county_first_case$firstDay))
   }
-  
+
   function(parms) {
     logp <- prior_weight * lprior(parms)
     if(verbose) {
@@ -287,11 +288,11 @@ gen_post <- function(prior_weight=NULL, fixed_parms=NULL, hparms=list(), verbose
 
 
 #' Find the symptomatic infection fraction
-#' 
+#'
 #' The fraction of infections that are symptomatic varies over the course of the
 #' scenario, so you must pick a range of values to average over.  If no range is
 #' specified, the default is days 80-93 (inclusive) from the start of the simulation.
-#' 
+#'
 #' @param modrun Model output from \code{\link{run_scenario}}
 #' @param trange Range of times to average symptomatic fraction over.  The endpoints
 #' are included.
@@ -300,12 +301,12 @@ fsympto <- function(modrun, trange=c(80,93))
 {
   ## silence warnings
   I <- Is <- time <- NULL
-  
+
   stopifnot(length(trange) == 2)
   if(inherits(trange, 'Date')) {
     trange <- as.integer(trange - as.Date('2020-01-01'))
   }
-  
+
   ## We have an fs column, but it's computed on a county-by county basis, and
   ## we want the aggregate over the state, which is easiest to get by recomputing
   ## fs from the aggregate data.
@@ -315,20 +316,20 @@ fsympto <- function(modrun, trange=c(80,93))
     dplyr::summarise(I=sum(I), Is=sum(Is)) %>%
     dplyr::mutate(fs = Is/(I+Is)) %>%
     dplyr::filter(time == floor(time))         # drop fractional initial time values.
-  
+
   mean(stateagg$fs)
 }
 
 #' Calculate the expected number of hospitalizations for a model
-#' 
+#'
 #' Calculate the expected number of hospitalizations by day, based on an assumed
 #' fraction of symptomatic patients that eventually go to the hospital.
-#' 
+#'
 #' To make this calculation, we have to convert this probability to a rate.  If
 #' the recovery rate is \eqn{\gamma = 1/D0}, then the hospitalization rate \eqn{r_h} that gives
-#' the desired probability \eqn{P_h} is 
+#' the desired probability \eqn{P_h} is
 #' \deqn{r_h = \gamma \frac{P_h}{1-P_h}}
-#' 
+#'
 #' @param ph Per-person hospitalization probability
 #' @param modout Model output from \code{\link{run_scenario}}
 #' @param mfadjust If \code{TRUE}, adjust for market fraction
@@ -337,24 +338,24 @@ fsympto <- function(modrun, trange=c(80,93))
 calc_nhosp <- function(ph, modout, mfadjust=TRUE)
 {
   rh <- ph/(1-ph) / modout$recoveryTime[1]
-  ## We're going to approximate the expected influx of hospital patients as 
+  ## We're going to approximate the expected influx of hospital patients as
   ## rh * PopSympto.  Theoretically we should be integrating the hospital population
-  ## alongside the other population differential equations, but this approximation is 
+  ## alongside the other population differential equations, but this approximation is
   ## close enough for our purposes.
   sympt <- modout[c('time','PopSympto')]
   if(mfadjust) {
     sympt[['PopSympto']] <- sympt[['PopSympto']] * modout[['marketFraction']]
   }
-  
+
   dplyr::group_by(sympt, time) %>%
     dplyr::summarise(expectedHosp = sum(PopSympto * rh))
 }
 
 #' Calculate the likelihood adjustment for number of hospital observations
-#' 
-#' The adjustment is 
+#'
+#' The adjustment is
 #' \deqn{L_h = \log(\int dp_h Beta(\alpha_h, \beta_h) \prod Pois(N_h, \lambda(p_h)))}
-#' 
+#'
 #' @param alpha First shape parameter in the prior for hospitalization fraction
 #' @param beta Second shape parameter in the prior for hospitalization fraction
 #' @param modout Model output from \code{\link{run_scenario}}
@@ -363,7 +364,7 @@ nhosp_likelihood <- function(alpha, beta, modout)
 {
   obs <- uva_covid_count[c('time','Admits')]
   modout <- dplyr::filter(modout, time %in% obs$time)
-  
+
   llsingle <- function(ph) {
     expectHosp <- calc_nhosp(ph, modout)
     cmpHosp <- dplyr::left_join(expectHosp, obs, by='time')
@@ -372,7 +373,7 @@ nhosp_likelihood <- function(alpha, beta, modout)
   llvector <- function(phvec) {
     sapply(phvec, llsingle)
   }
-  
-  
+
+
   log(integrate(llvector, 0,1)$value)
 }
--- R/prior.R
+++ R/prior.R
@@ -1,14 +1,27 @@
 
+## distribution parameters for priors
+hprior <- list(
+    D0gshp = 8, D0grate = 2,
+    A0gshp = 8, A0grate = 2,
+    Tsgshp = 8, Tsgrate = 2,
+    I0lmu = 2, I0lsig = 1,
+    MEgshp = 1, MEgrate = 2,
+    b0lmu = 4, b0lsig = 0.5,
+    b1exprate = 1,
+
+    betalmu = -0.7, betalsig = 1
+)
+
 #' Generate a log prior function
-#' 
+#'
 #' This function returns a a function that given an input set of parameters evaluates
 #' the log-prior probability density for the parameters.  Eventually this function
 #' will be able to take hyperparameter arguments that influence the details of the
 #' priors, but for now they're fixed.
-#' 
+#'
 #' The parameters currently recognized are described in \code{\link{gen_likelihood}}.
-#' If there are any parameters not in that list present, an error will be raised.  
-#' 
+#' If there are any parameters not in that list present, an error will be raised.
+#'
 #' @param hparms Named vector of hyperparameters, described in \code{\link{gen_post}}
 #' @param verbose If \code{TRUE}, print out additional diagnostic information.
 #' @return A function that takes a named vector of parameters and returns the log-prior
@@ -16,69 +29,79 @@
 #' @export
 gen_prior <- function(hparms, verbose=FALSE)
 {
-  parm_names <- c('eta','xi', 'zeta', 'D0', 'A0', 'Ts', 'I0', 'mask_effect', 'b0', 'b1')
+  common_parm_names <- c('D0', 'A0', 'Ts', 'I0', 'mask_effect', 'b0', 'b1')
   function(parms) {
     stopifnot(!is.null(names(parms)))
-    if(any(!names(parms) %in% parm_names)) {
-      badparms <- names(parms)[!names(parms) %in% parm_names]
+
+    lbeta <- extract_beta_parms(names(parms), 'logical')
+    beta_parms <- parms[lbeta]
+    common_parms <- parms[!lbeta]
+
+    if(any(!names(common_parms) %in% common_parm_names)) {
+      badparms <- names(common_parms)[!names(common_parms) %in% common_parm_names]
       stop('unrecognized parameters: ', paste(badparms, collapse=', '))
     }
 
     logps <- c(
-      dgamma(parms[c('A0', 'Ts', 'D0')], 
-             c(8,8, 14), 
-             c(2,2, 2), log=TRUE),
-      dnorm(parms['eta'], -0.7, 1, log=TRUE),
-      dlnorm(parms['xi'], -0.5, 2, log=TRUE),
-      dgamma(parms['zeta'], 1, 1, log=TRUE),         # mobility effect expected to be positive
-      dlnorm(parms['I0'], 2, 1, log=TRUE),
-      dgamma(-parms['mask_effect'], 1, 2, log=TRUE),  # mask effect expected to be negative
-      dlnorm(parms['b0'], hparms[['bmulog']], hparms[['bsiglog']], log=TRUE),
-      dexp(parms['b1'], 1, log=TRUE)
+      dlnorm(beta_parms, hprior$betalmu, hprior$betalsig, log=TRUE),
+      dgamma(common_parms['A0'], hprior$A0gshp, hprior$A0grate, log=TRUE),
+      dgamma(common_parms['Ts'], hprior$Tsgshp, hprior$Tsgrate, log=TRUE),
+      dgamma(common_parms['D0'], hprior$D0gshp, hprior$D0grate, log=TRUE),
+      dlnorm(common_parms['I0'], hprior$I0lmu, hprior$I0lsig, log=TRUE),
+      dgamma(-common_parms['mask_effect'], hprior$MEgshp, hprior$MEgrate, log=TRUE),  # mask effect expected to be negative
+      dlnorm(common_parms['b0'], hprior$b0lmu, hprior$b0lsig, log=TRUE),
+      dexp(common_parms['b1'], hprior$b1exprate, log=TRUE)
     )
     if(verbose) {
-      pnames <- c('A0:\t', 'Ts:\t',  'D0:\t', 
-                  'eta', 'xi', 'zeta',
-                  'b:\t', 'I0:\t', 'mask_effect:\t')
-      prvals <- signif(logps, 3)
-      msg <- paste(pnames, prvals, collapse='\n')
-      totmsg <- paste('total:\t', signif(sum(logps, na.rm=TRUE), 4))
-      message('Prior contributions:\n', msg, '\n----------------\n', totmsg)
+        pnames <- c(paste0(names(beta_parms),'\t'),
+                    'A0:\t', 'Ts:\t',  'D0:\t',
+                    'I0:\t', 'mask_effect:\t',
+                    'b0:\t', 'b1:\t')
+        prvals <- signif(logps, 3)
+        msg <- paste(pnames, prvals, collapse='\n')
+        totmsg <- paste('total:\t', signif(sum(logps, na.rm=TRUE), 4))
+        message('Prior contributions:\n', msg, '\n----------------\n', totmsg)
     }
     sum(logps, na.rm = TRUE)
   }
 }
 
 #' Evaluate the quantile for the multidimensional df defined by the priors
-#' 
+#'
 #' Given a vector of probabilities, find the corresponding quantile vector assuming
 #' an independence copula and margins as described in \code{\link{gen_prior}}.
-#' 
+#'
 #' @section TO DO:
-#' 
+#'
 #' This function is currently inflexible with respect to the parameters included
 #' in the calculation (because of the fixed list of parameter names).  Fix this
 #' so that we can pass p as a named list.
-#' 
+#'
 #' @param p A vector of probabilities
 #' @param hparms  Named vector of hyperparameters, described in \code{\link{gen_post}}
 #' @export
 qprior <- function(p, hparms=list()) {
   hparms <- fill_defaults(hparms, default_hparms)
-  parm_names <- c('eta', 'xi', 'zeta', 'D0', 'A0', 'Ts', 'I0', 'mask_effect', 'b0','b1')
-  stopifnot(length(p) == length(parm_names))
-  
+  common_parm_names <- c('D0', 'A0', 'Ts', 'I0', 'mask_effect', 'b0','b1')
+
+  lbeta <- extract_beta_parms(names(p), 'logical')
+  beta_p <- p[lbeta]
+  common_p <- p[!lbeta]
+
+  stopifnot(length(common_p) == length(common_parm_names))
+
+  beta_vals <- qlnorm(beta_p, hprior$betalmu, hprior$betalsig)
+  names(beta_vals) <- names(p)[lbeta]
+
   ## Return a named vector of parameters
   c(
-    eta = qnorm(p[1], -0.7, 1),
-    xi = qlnorm(p[2], -0.5, 2),
-    zeta = qgamma(p[3], 1, 1),
-    D0 = qlnorm(p[4], 2, 0.5),
-    A0 = qgamma(p[5], 8, 2),
-    Ts = qgamma(p[6], 8, 2),
-    I0 = qlnorm(p[7], 2, 1),
-    mask_effect = -qgamma(p[8], 1, 2),
-    b0 = qlnorm(p[9], hparms[['bmulog']], hparms[['bsiglog']]),
-    b1 = qexp(p[10], 1)
+    beta_vals,
+    D0 = qgamma(common_p[['D0']], hprior$D0gshp, hprior$D0grate),
+    A0 = qgamma(common_p[['A0']], hprior$A0gshp, hprior$A0grate),
+    Ts = qgamma(common_p[['Ts']], hprior$Tsgshp, hprior$Tsgrate),
+    I0 = qlnorm(common_p[['I0']], hprior$I0lmu, hprior$I0lsig),
+    mask_effect = -qgamma(common_p[['mask_effect']], hprior$MEgshp, hprior$MEgrate),
+    b0 = qlnorm(common_p[['b0']], hprior$b0lmu, hprior$b0lsig),
+    b1 = qexp(common_p[['b1']], hprior$b1exprate)
   )
 }
