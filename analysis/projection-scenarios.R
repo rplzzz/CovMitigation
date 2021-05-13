fdc18086f3e8d1946fa2f7146a62936a8c336566
Robert Link <rl8z@virginia.edu>
2021-05-13 12:35:58 -0400

    Update with hospitalizations through 2021-05-09

--- analysis/projection-scenarios.R
+++ analysis/projection-scenarios.R
@@ -2,15 +2,15 @@ library(CovMitigation)
 library(ggplot2)
 library(dplyr)
 
-modeldate <- '2021-04-25'
+modeldate <- '2021-05-02'
 modeldir <- paste0('analysis/filter-updates.', modeldate)
 message('model date: ', modeldate, '\tmodel dir: ', modeldir)
 jan01 <- as.Date('2020-01-01')
 nov01 <- as.Date('2020-11-01')
 months <- seq(as.Date('2020-11-01'), as.Date('2021-10-01'), by='month')
 times <- as.numeric(months - jan01)
-enddate <- as.numeric(as.Date('2021-08-31') - jan01)
-lastobsdate <- as.Date('2021-04-28')
+enddate <- as.numeric(as.Date('2021-09-30') - jan01)
+lastobsdate <- as.Date('2021-05-06')
 modelmonth <- lubridate::as.period(lubridate::interval(nov01, as.Date(modeldate)))@month + 1
 
 ## Start of the scenario adjustment
